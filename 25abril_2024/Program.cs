7a741a4d91cc598932588ec6afaa8420b9a201a0
carli <carli@DEFII-LAPTOP>
2024-04-25 08:10:43 -0600

    Add project files.

---
+++ 25abril_2024/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25abril_2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
